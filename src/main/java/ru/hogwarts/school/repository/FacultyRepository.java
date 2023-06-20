35afb2e25ee2173b1b43b9f8e39a9cb89b6b6056
shard <alisher.sharipov2@mail.ru>
2023-06-21 03:23:36 +0600

    Done

--- src/main/java/ru/hogwarts/school/repository/FacultyRepository.java
+++ src/main/java/ru/hogwarts/school/repository/FacultyRepository.java
@@ -9,5 +9,5 @@ import java.util.Collection;
 public interface FacultyRepository extends JpaRepository<Faculty, Long> {
     Collection<Faculty> findByNameLikeIgnoreCase(String name);
     Collection<Faculty> findByColorIsLikeIgnoreCase(String color);
-    Faculty findFacultyByStudentsId(int id);
+    Faculty findFacultyByStudentsId(Long students_id);
 }
